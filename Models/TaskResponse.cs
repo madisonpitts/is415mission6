51ccd49ac5f6b747db88de86aa89278541fa3060
benjasch97 <schneider.benjamina97@gmail.com>
2022-02-08 15:21:51 -0700

    Ben's initial changes

---
+++ Models/TaskResponse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mission6.Models
+{
+    public class TaskResponse
+    {
+        [Key]
+        [Required]
+        public int TaskID { get; set; }
+        [Required]
+        public string TaskName { get; set; }
+        public string DueDate { get; set; }
+        [Required]
+        public int Quadrant { get; set; }
+        public bool Completed { get; set; }
+        
+        public int CategoryID { get; set; }
+        public Category CategoryName { get; set; }
+
+
+    }
+}
---
+++ Models/TaskResponseContext.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mission6.Models
+{
+    public class TaskResponseContext : DbContext
+    {
+        // Constructor
+        public TaskResponseContext(DbContextOptions<TaskResponseContext> options) : base(options)
+        {
+            // Leaving this blank for now
+        }
+
+        public DbSet<TaskResponse> Responses { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<Category>().HasData(
+                new Category { CategoryID = 1, CategoryName = "Home" },
+                new Category { CategoryID = 2, CategoryName = "School" },
+                new Category { CategoryID = 3, CategoryName = "Work" },
+                new Category { CategoryID = 4, CategoryName = "Church" }
+            );
+
+            mb.Entity<TaskResponse>().HasData(
+            
+                new TaskResponse
+                {
+                    TaskID = 1,
+                    TaskName = "Read Book of Mormon",
+                    DueDate = "01/01/2022",
+                    Quadrant = 2,
+                    CategoryID = 4,
+                    Completed = false
+                },
+                new TaskResponse
+                {
+                    TaskID = 2,
+                    TaskName = "Watch Book of Boba Fett",
+                    DueDate = "02/09/2022",
+                    Quadrant = 4,
+                    CategoryID = 1,
+                    Completed = false
+                }
+            );
+        }
+    }
+}
